e64bf06700531006518fc57444cd65e5ab2a775c
faint32 <faint32@users.noreply.github.com>
2025-11-30 17:02:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,7 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.254/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.2.1/192.168.1.254/g' package/base-files/files/bin/config_generate
 
 # Modify default password
 sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
